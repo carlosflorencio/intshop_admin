0eaf839c7ef5e711b42d92ac20477647665d3546
Carlos Florencio <carlosmflorencio@gmail.com>
2016-05-23 23:14:34 +0100

    Added develop apis to have data to develop the app, in the future replace for real data

---
+++ angular/shared/ApiDevelop.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+ |--------------------------------------------------------------------------
+ | Api Develop Service
+ |--------------------------------------------------------------------------
+ */
+angular.module('intshop').factory('DevelopRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setBaseUrl('http://intshop-admin.dev:8080');
+    });
+});
+
+angular.module('intshop').service('ApiDevelop', function (DevelopRestangular) {
+    return {
+        getShopDetailsPromise: function(id) {
+            return DevelopRestangular.one('api').one("shop-details.json").get();
+        },
+        getShopLastOrdersPromise: function(id, limit) {
+            return DevelopRestangular.one('api').one("shop-last-orders.json").get();
+        }
+    }
+});
